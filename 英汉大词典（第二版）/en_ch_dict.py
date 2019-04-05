f9dbb05885ea0523108e5097c8c0a682507dbc56
vivo <mahavivo@outlook.com>
2019-04-05 17:24:59 +0800

    edited

--- 英汉大词典（第二版）/en_ch_dict.py
+++ 英汉大词典（第二版）/en_ch_dict.py
@@ -1,4 +1,8 @@
-file_src = "/users/vivo/desktop/英汉大词典（第二版）.txt"
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+file_src = "/users/vivo/desktop/英汉大词典_INIT.txt"
 
 file_dst = "/users/vivo/desktop/英汉大词典_edited.txt"
 
