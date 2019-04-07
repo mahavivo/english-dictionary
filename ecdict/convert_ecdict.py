c698fc7f468eca4f44169c6cc6129fa5d68c18ff
vivo <mahavivo@outlook.com>
2019-04-07 22:15:52 +0800

    init

---
+++ ecdict/convert_ecdict.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# csv数据转换成SQLite数据库格式，不直接放ecdict.db，是因为会膨胀很大。
+# 此数据包含770611条记录，来自 https://github.com/skywind3000/ECDICT
+
+import stardict
+
+stardict.convert_dict("./ecdict.db", "./ecdict.csv") 
