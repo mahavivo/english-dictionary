56881b1dee555dedef254f737731fc2b3c0f943c
vivo <mahavivo@outlook.com>
2019-04-15 18:32:35 +0800

    edit

--- ecdict/convert_dict.py
+++ ecdict/convert_dict.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # csv数据转换成SQLite数据库格式，不直接放ecdict.db，是因为会膨胀很大。
@@ -6,4 +6,4 @@
 
 import stardict
 
-stardict.convert_dict("./ecdict.db", "./ecdict.csv") 
+stardict.convert_dict('./ecdict.db', './ecdict.csv')
--- 英汉大词典（第二版）/en_ch_dict.py
+++ 英汉大词典（第二版）/en_ch_dict.py
@@ -2,14 +2,14 @@
 # -*- coding: utf-8 -*-
 
 
-file_src = "/users/vivo/desktop/英汉大词典_INIT.txt"
-file_dst = "/users/vivo/desktop/英汉大词典_edited.txt"
+file_src = '/users/vivo/desktop/英汉大词典_INIT.txt'
+file_dst = '/users/vivo/desktop/英汉大词典_edited.txt'
 
 
 def parser(text):
-    # "派生"标记【卍】 和 "习语"标记【★】尚未处理
-    outtext = text.replace("■", "").replace("ⓐ", "").\
-        replace("⏎", "").replace("▶", "").replace("➜", "")
+    # ”派生“标记【卍】 和 ”习语”标记【★】尚未处理
+    outtext = text.replace('■', '').replace('ⓐ', '').\
+        replace('⏎', '').replace('▶', '').replace('➜', '')
 
     return outtext
 
@@ -19,8 +19,7 @@ all_entry = []
 with open(file_src, 'r') as f:
     full_text = f.read()
 
-
-    entry_list = full_text.split("————————————")
+    entry_list = full_text.split('————————————')
     for entry in entry_list:
         if entry:
             row_list = []
@@ -28,13 +27,14 @@ with open(file_src, 'r') as f:
             for line in line_list:
                 line_out = parser(line)
                 row_list.append(line_out)
-            # row_list = [x for x in row_list if x != ""]
-            new_entry = '\n'.join(row_list)  #不分行显示则用空格代替"\n"
+            # row_list = [x for x in row_list if x != '']
+            new_entry = '\n'.join(row_list)  #不分行显示则用空格代替'\n'
             new_entry = new_entry.strip()
 
         all_entry.append(new_entry)
 
+
 with open(file_dst, 'w') as f_out:
     for each in all_entry:
-        # each = " ".join(each.splitlines())
+        # each = ' '.join(each.splitlines())
         f_out.write('\n\n————————————\n' + each)
