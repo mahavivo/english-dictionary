084a9b4d8147368d4e2529f1b4a0e7ed7acf813a
vivo <mahavivo@outlook.com>
2019-04-16 17:52:29 +0800

    edit

--- ecdict/convert_dict.py
+++ ecdict/convert_dict.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 # csv数据转换成SQLite数据库格式，不直接放ecdict.db，是因为会膨胀很大。
-# 此数据包含770611条记录，来自 https://github.com/skywind3000/ECDICT
+# 此数据包含40万条记录，提取自共有770611条记录的ecdict.csv（出处 https://github.com/skywind3000/ECDICT）,
+# 删除了所有短语和没有汉语释义的词条，只保留word、phonetic、translation三个字段。
 
 import stardict
 
-stardict.convert_dict('./ecdict.db', './ecdict.csv')
+stardict.convert_dict('./minidict(400k).db', './minidict(400k).csv')
