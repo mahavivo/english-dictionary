0ebb47d91a010777a0271612c10517eb53b44630
vivo <mahavivo@outlook.com>
2019-04-18 14:54:55 +0800

    edit

--- 英汉大词典（第二版）/en_ch_dict.py
+++ 英汉大词典（第二版）/en_ch_dict.py
@@ -11,15 +11,15 @@ def parser(text):
 
 
 def main():
-    file_src = "/users/vivo/desktop/英汉大词典（第二版）.txt"
+    file_src = "/users/vivo/desktop/英汉大词典_INIT.txt"
     file_dst = "/users/vivo/desktop/英汉大词典_edited.txt"
 
     all_entry = []
 
     with open(file_src, 'r') as f:
         full_text = f.read()
-
         entry_list = full_text.split("————————————")
+
         for entry in entry_list:
             if entry:
                 row_list = []
@@ -31,7 +31,7 @@ def main():
                 new_entry = '\n'.join(row_list)  # 不分行显示则用空格代替"\n"
                 new_entry = new_entry.strip()
 
-            all_entry.append(new_entry)
+                all_entry.append(new_entry)
 
     with open(file_dst, 'w') as f_out:
         for each in all_entry:
